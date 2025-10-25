a0d0a6b35b0b1fe6032068a432a11cccd9f6e20a
salom <7alimbayevv@gmail.com>
2025-10-25 11:43:12 +0500

    js qodlari

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import TelegramBot from "node-telegram-bot-api";
 const TOKEN = ""
-const bot = new TelegramBot(TOKEN, {polling: true})
-bot.on("message", function (msg){
+const bot = new TelegramBot(TOKEN, { polling: true })
+bot.on("message", function (msg) {
     const chatId = msg.chat.id;
     const text = msg.text;
     console.log(msg)
     bot.sendMessage(chatId, `${text}`)
 })
+
+console.log("Bot ishga tushdi...")
